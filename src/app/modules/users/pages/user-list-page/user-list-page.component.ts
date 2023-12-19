8382b326bfb01d1d7cea95eb0ea3e97f7a9e999c
Adrian <alextravc.342@gmail.com>
2023-12-19 10:15:38 -0500

    [ADD]: user list page skeleton

---
+++ src/app/modules/users/pages/user-list-page/user-list-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list-page',
+  templateUrl: './user-list-page.component.html',
+  styles: [
+  ]
+})
+export class UserListPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
