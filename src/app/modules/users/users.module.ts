4317bb3c2fb0b06e71ff1e136e883ebdabb60e6f
Adrian <alextravc.342@gmail.com>
2023-12-19 10:24:45 -0500

    [ADD]: some imports needed in users module

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -1,17 +1,24 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { InlineSVGModule } from 'ng-inline-svg-2';
 
-import { UsersRoutingModule } from './users-routing.module';
 import { UserListPageComponent } from './pages/user-list-page/user-list-page.component';
-
+import { UsersRoutingModule } from './users-routing.module';
 
 @NgModule({
-  declarations: [
-    UserListPageComponent
-  ],
+  declarations: [UserListPageComponent],
   imports: [
     CommonModule,
-    UsersRoutingModule
-  ]
+    UsersRoutingModule,
+
+    HttpClientModule,
+    NgbModule, // bootstrap
+    NgbModalModule, // bootstrap
+    ReactiveFormsModule,
+    InlineSVGModule,
+  ],
 })
-export class UsersModule { }
+export class UsersModule {}
